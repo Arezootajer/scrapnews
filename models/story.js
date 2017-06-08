408428756d7555d0b151ef50854da8be7127f9d6
Arezoo Tajerzadeh <Arezoo Tajerzadeh>
2017-06-07 18:25:11 -0700

    upload

---
+++ models/story.js
@@ -0,0 +1,35 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var StorySchema = new Schema({
+ 
+  title: {
+    type: String,
+    required: true
+  },
+ 
+  link: {
+    type: String,
+    required: true
+  },
+
+  saved: {
+    type: Boolean,
+    default: false
+  },
+
+  // This only saves one note's ObjectId, ref refers to the Note model
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// Create the Article model with the ArticleSchema
+var Story = mongoose.model("Story", StorySchema);
+
+// Export the model
+module.exports = Story;
